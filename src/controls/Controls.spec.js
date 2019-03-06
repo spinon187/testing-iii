f1c0afc3d627d645288899782cc97f329138a892
spinon187 <spinon187@gmail.com>
2019-03-06 17:53:01 -0600

    standup push, nothing works

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,24 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Controls from './Controls';
+ 
+
+describe('toggleLocked()', () => {
+    it('toggles the lock', () => {
+        const toggleLocked = jest.fn();
+        const {getByText} = render(<Controls toggleLocked={toggleLocked}/>);
+        fireEvent.click(getByText(/Lock Gate/i));
+        expect(toggleLocked).toHaveBeenCalled();
+    })
+})
+
+describe('toggleClosed()', () => {
+    it('opens or closes the gate', () => {
+        const toggleClosed = jest.fn();
+        const {getByText} = render(<Controls toggleClosed={toggleClosed}/>);
+        const button = getByText(/Close Gate/i);
+        fireEvent.click(button);
+        expect(toggleClosed).toHaveBeenCalled();
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,3 +2,11 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import Display from './Display';
+
+// describe('<Display />', () => {
+//     it(
+
+//     )
+// })
+
+test.skip('skip', () => {})
